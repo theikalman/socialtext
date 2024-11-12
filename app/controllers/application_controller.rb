089fa61d0a54988a2db571da34b8cd50cae404bc
AjiYakin <ajiyakin91@gmail.com>
2024-11-12 16:48:29 +0700

    check if user login before do any action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :check_user_login
+
   private
 
   def check_user_login
